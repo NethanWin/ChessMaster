25ff5c678cd90458c79e0818430afd67ff17ec1f
Nitsan <nitsan.weingart@gmail.com>
2022-03-27 13:17:35 +0300

    server init

---
+++ Server/Branch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Branch
+{
+    public string board;
+    public bool isMax;
+    public List<Branch> next;
+
+    public Branch(string board, bool isMax = true)
+    {
+        this.board = board;
+        this.isMax = isMax;
+        this.next = Chess.GetNextGen(this);
+    }
+
+    public override string ToString()
+    {
+        string str = board;
+        if (!IsEmptyList(next))
+        {
+            str += "\n";
+            foreach (Branch b in next)
+                if (!IsEmptyList(next))
+                    str += b.ToString() + " - ";
+        }
+        return str;
+    }
+    /*public (int, List<Branch>) GetBestMoveAndScore()
+    {
+        
+    }*/
+    public static bool IsEmptyList<T>(List<T> list)
+    {
+        if (list == null)
+        {
+            return true;
+        }
+
+        return !list.Any();
+    }
+}
---
+++ Server/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Point
+{
+    public byte x;
+    public byte y;
+
+    public Point(byte x, byte y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public Point((byte, byte) p)
+    {
+        this.x = p.Item1;
+        this.y = p.Item2;
+    }
+    public (byte, byte) GetPoint()
+    {
+        return (x, y);
+    }
+}
